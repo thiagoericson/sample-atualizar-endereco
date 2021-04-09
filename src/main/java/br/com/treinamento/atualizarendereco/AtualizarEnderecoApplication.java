8def84830a012d029789368c09d48fb17fb9bfe4
Thiago Cabral <ericson.thiago@gmail.com>
2021-04-09 16:39:07 -0300

    First commit

---
+++ src/main/java/br/com/treinamento/atualizarendereco/AtualizarEnderecoApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.atualizarendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtualizarEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtualizarEnderecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinamento/atualizarendereco/AtualizarEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.atualizarendereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtualizarEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
